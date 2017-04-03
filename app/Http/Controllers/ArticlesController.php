8cbad8c525732a431d7f939d69dcfa9b7b69c37c
Zac Laudick <zlaudick@nmsu.edu>
2017-04-02 20:56:19 -0600

    article model, view, and controller

---
+++ app/Http/Controllers/ArticlesController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Article;
+use App\Http\Requests;
+
+class ArticlesController extends Controller
+{
+    public function index() {
+        $articles = Article::all();
+
+        return view('articles.index', compact('articles'));
+    }
+
+    public function show($id) {
+        $article = Article::findOrFail($id);
+
+        return view('articles.show', compact('article'));
+    }
+}
