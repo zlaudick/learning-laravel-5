2cbd0cd427fb0e22c08e5bb7ea748cbc739535a2
Zac Laudick <zlaudick@nmsu.edu>
2017-04-02 21:51:12 -0600

    added form for adding new articles

--- app/Http/Controllers/ArticlesController.php
+++ app/Http/Controllers/ArticlesController.php
@@ -2,14 +2,15 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
 use App\Article;
 use App\Http\Requests;
+use Carbon\Carbon;
+use Request;
 
 class ArticlesController extends Controller
 {
     public function index() {
-        $articles = Article::all();
+        $articles = Article::latest('published_at')->get();
 
         return view('articles.index', compact('articles'));
     }
@@ -18,5 +19,18 @@ class ArticlesController extends Controller
         $article = Article::findOrFail($id);
 
         return view('articles.show', compact('article'));
+    }
+
+    public function create() {
+        return view('articles.create');
+    }
+
+    public function store() {
+        $input = Request::all();
+        $input['published_at'] = Carbon::now();
+
+        Article::create($input);
+
+        return redirect('articles');
     }
 }
